c457a03492914fb03c01531aad24b2807697f552
Michal Cichowski <mike@silentmike.pl>
2024-05-19 22:24:51 +0200

    add tye

---
+++ src/Kafka.Poc.Server/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿namespace Kafka.Poc.Server.Controllers;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[AllowAnonymous]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        this.logger = logger;
+    }
+
+    [Route(""), HttpGet, ApiExplorerSettings(IgnoreApi = true)]
+    public RedirectResult Index()
+    {
+        this.logger.LogInformation("Redirect to Swagger");
+
+        return this.Redirect("swagger/index.html");
+    }
+}
